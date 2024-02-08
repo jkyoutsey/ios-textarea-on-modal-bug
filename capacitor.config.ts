80a4572d7d69d2e1be1d6bf15fea739973e3b459
Jared Youtsey <github@fivedice.net>
2024-02-08 12:59:51 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ios-textarea-on-modal-bug',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
