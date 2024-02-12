5177ff1cd72c09309bbe4ce129bd0b804e922836
Jared Youtsey <github@fivedice.net>
2024-02-12 07:20:59 -0600

    can't scroll in a textarea on a modal

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { IonButton, IonTextarea, IonModal } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-explore-container',
   templateUrl: './explore-container.component.html',
   styleUrls: ['./explore-container.component.scss'],
   standalone: true,
+  imports: [IonTextarea, IonButton, IonModal],
 })
 export class ExploreContainerComponent {
   @Input() name?: string;
